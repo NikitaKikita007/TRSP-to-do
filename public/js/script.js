33ee6fa1b7a9859b2084f645ef42f8e000dd41de
NikitaKikita07 <119515103+NikitaKikita007@users.noreply.github.com>
2024-12-26 15:33:51 +0200

    node

---
+++ public/js/script.js
@@ -0,0 +1,10 @@
+const button = document.querySelector('#button');
+const titleH = document.querySelector('.title');
+
+button.addEventListener('click',() => {
+    titleH.innerHTML = titleH.innerHTML+"🤘😎🤘ROBIT!!!!!🤘😎🤘"
+
+});
+
+
+console.log(`${Math.random()*100}px`)
